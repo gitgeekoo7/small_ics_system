542913c8b2bfb9143e601521b798f7374bfe8afc
ming19 <mingwenqiang@cestco.cn>
2019-04-10 18:19:59 +0800

    跨域配置...

---
+++ small_ics_system/src/main/java/com/cesecsh/small_ics_system/config/CorsConfigurer.java
@@ -0,0 +1,17 @@
+package com.cesecsh.small_ics_system.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfigurer implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedHeaders("*")
+                .maxAge(60000);
+    }
+}
